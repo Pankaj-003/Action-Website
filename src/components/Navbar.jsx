9c51242b939b7e37666be6e78a3d5f1bea53f729
Pankaj-003 <pankajbiswasorginal@gmail.com>
2025-03-23 18:28:31 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ Bootstrap JS
 const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
   const handleLogout = () => {
     setIsAuthenticated(false);
